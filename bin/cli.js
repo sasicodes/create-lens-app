0f1847224f8388157302d1959add2d01e0c6b894
Sasi <me@sasi.codes>
2022-12-27 14:31:54 +0530

    chore: add inquirer for templates

--- bin/cli.js
+++ bin/cli.js
@@ -7,26 +7,39 @@ const fs = require('fs');
 const path = require('path');
 const arg = require('arg');
 const chalk = require('chalk');
+const inquirer = require('inquirer');
 
 const validTemplates = ["vite-ts", "vite-js"]
 
 const args = arg({
     '--help': Boolean,
     '--template': String,
+    '--list': Boolean,
     '-h': '--help',
     '-t': '--template',
+    '-l': '--list',
 });
+
+if (args['--list']) {
+    console.log('\n')
+    validTemplates.map(t => console.log(chalk.green(`${t}`)))
+    console.log('\n')
+    process.exit(0)
+}
 
 if (args['--help'] || (!args._[0])) {
     console.log(chalk`
     {bold.cyan create-lens-app} - Create Lens App in one command ⚡
+
     {bold USAGE}
       {bold $} {cyan create-lens-app} --help
       {bold $} {cyan create-lens-app} {underline my-app}
-      {bold $} {cyan create-lens-app} {underline my-app} [--template {underline template_name}]
+      {bold $} {cyan create-lens-app} {underline my-app} [--template {underline <template-name>}]
+      
     {bold OPTIONS}
-      --help,     -h                      shows this help message
-      --template,  -t {underline template_name}  sets the template
+      --help,      -h        shows this help message
+      --template,  -t        sets the template
+      --list,      -l        lists all template
   `);
     process.exit(0);
 }
@@ -36,47 +49,61 @@ const runCommand = command => {
         execSync(`${command}`, { stdio: 'inherit' })
     } catch (error) {
         console.log(`Failed to execute ${command}`, error)
-        return false
+        process.exit(1)
     }
 }
-const currentDir = process.cwd();
 
-// The first argument will be the project name.
+const currentDir = process.cwd();
 const projectName = path.join(currentDir, args._[0]);
 
-const templateName = args['--template'] || 'vite-js';
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'template',
+            message: 'Choose a template: ',
+            choices: validTemplates,
+        },
+    ])
+    .then(answers => {
+        const templateName = args['--template'] || answers.template || 'vite-js';
 
-if (!validTemplates.includes(templateName)) return console.log(`❗❗❗ Template not found: ${templateName}`)
+        if (!validTemplates.includes(templateName)) {
+            console.log(`❗❗❗ Template not found: ${templateName}`)
+            process.exit(1)
+        }
 
-console.log("🌿 Creating Lens project...")
-// Create a project directory with the project name.
-const projectDir = path.resolve(currentDir, projectName);
-fs.mkdirSync(projectDir, { recursive: true });
+        console.log("\n\n🌿 Creating Lens project")
 
-console.log(`📦 Using template ${templateName}`);
-const templateDir = path.resolve(__dirname, `../templates/${templateName}`);
-fs.cpSync(templateDir, projectDir, { recursive: true, force: true });
+        const projectDir = path.resolve(currentDir, projectName);
+        fs.mkdirSync(projectDir, { recursive: true });
 
-fs.renameSync(
-    path.join(projectDir, '_gitignore'),
-    path.join(projectDir, '.gitignore')
-); 
+        console.log(`📦 Using template ${templateName}`);
+        const templateDir = path.resolve(__dirname, `../templates/${templateName}`);
+        fs.cpSync(templateDir, projectDir, { recursive: true, force: true });
 
-const projectPackageJson = require(path.join(projectDir, 'package.json'));
+        fs.renameSync(
+            path.join(projectDir, '_gitignore'),
+            path.join(projectDir, '.gitignore')
+        );
 
-// Update the project's package.json with the new project name
-projectPackageJson.name = projectName;
+        const projectPackageJson = require(path.join(projectDir, 'package.json'));
 
-fs.writeFileSync(
-    path.join(projectDir, 'package.json'),
-    JSON.stringify(projectPackageJson, null, 2)
-);
+        projectPackageJson.name = projectName;
 
-// Run `npm install` in the project directory to install
-// the dependencies.
-console.log(`🌀 Installing dependencies for ${projectName}`)
-const installDepsCommand = `cd ${projectName} && npm install`
-runCommand(installDepsCommand)
+        fs.writeFileSync(
+            path.join(projectDir, 'package.json'),
+            JSON.stringify(projectPackageJson, null, 2)
+        );
 
-console.log("\nOK BLOOMER 🌿 Your app is ready!");
-console.log(`\nCreated lens app at ${projectDir}`);
+        console.log(`🌀 Installing dependencies for ${projectName}`)
+        const installDepsCommand = `cd ${projectName} && npm install`
+        runCommand(installDepsCommand)
+
+        console.log(chalk`\n{bold.green OK BLOOMER} 🌿 Your app is ready!`);
+        console.log(chalk.gray(`\nCreated Lens project at ${projectDir}`));
+    })
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
